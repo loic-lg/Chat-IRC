421d81a0deb5b5ae0be91dc0904f7c50b246a5a3
loic <loic.le-gal@epitech.eu>
2025-02-27 12:48:08 +0100

    clean code

--- chat-app/backend/test-unit/test-unit.js
+++ chat-app/backend/test-unit/test-unit.js
@@ -11,8 +11,7 @@ const TEST_UNIT_EMAIL = "legal.loic@gmail.com";
 const runTest = async () => {
   try {
     
-    ///// 
-     
+    // Création du compte
     console.log("Création du compte...");
     const signupResponse = await axios.post(`${BASE_URL}/auth/signup`, {
       fullName: TEST_NAME,
@@ -22,7 +21,7 @@ const runTest = async () => {
     console.log("Utilisateur créé :", signupResponse.data);
     
 
-    ///
+    // Connexion
     console.log("Connexion...");
     const loginResponse = await axios.post(`${BASE_URL}/auth/login`, {
       email: TEST_EMAIL,
@@ -38,14 +37,14 @@ const runTest = async () => {
       headers: { Cookie: token },
     });
 
-    ////
+    //Test envoi de message
     console.log("Recherche du compte 'test-unit'...");
     const usersResponse = await api.get(`/messages/users`);
     const testUnitUser = usersResponse.data.find(user => user.email === TEST_UNIT_EMAIL);
     if (!testUnitUser) throw new Error("Le compte 'test-unit' est introuvable.");
     console.log("Compte 'test-unit' trouvé :", testUnitUser);
 
-    ////
+    // Envoi du message
     console.log("Envoi du message à 'test-unit'...");
     const messageResponse = await api.post(`/messages/send/${testUnitUser._id}`, {
       text: `${TEST_NAME} : test-unit réussi.`,
@@ -53,13 +52,12 @@ const runTest = async () => {
     });
     console.log("Message envoyé :", messageResponse.data);
 
-    ////
+    // Déconnexion
     console.log("Déconnexion...");
     await api.post(`/auth/logout`);
     console.log("Déconnecté.");
 
-
-    ////
+    // résultat
     console.log("Test terminé !");
   } catch (error) {
     console.error("Erreur pendant le test :", error.response?.data || error.message);
